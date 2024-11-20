74dca5af2adb39dae2bf5753adc8649717f4ba6f
thkinh <139554989+thkinh@users.noreply.github.com>
2024-11-20 13:23:06 +0700

    add screen detect

---
+++ app/src/main/java/com/example/doan/FragmentSensorData.java
@@ -0,0 +1,22 @@
+package com.example.doan;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragmentSensorData extends Fragment {
+
+    public FragmentSensorData() {
+        // Required empty public constructor
+    }
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflate the fragment layout
+        return inflater.inflate(R.layout.fragment_sensor_data, container, false);
+    }
+}
