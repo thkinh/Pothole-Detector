7575fa203d9cde14bef6027c5d632582e3c343e9
Nguyễn Hữu Tài <huutai.network@gmail.com>
2024-12-13 00:49:42 +0700

    Hiển thị bản đồ Mapbox và vị trí hiện tại

--- app/src/main/java/com/example/doan/FragmentMap.java
+++ app/src/main/java/com/example/doan/FragmentMap.java
@@ -1,244 +1,133 @@
 package com.example.doan;
 
-import android.Manifest;
-import android.annotation.SuppressLint;
-import android.content.Context;
-import android.content.Intent;
+
+import static com.mapbox.maps.plugin.gestures.GesturesUtils.getGestures;
+import static com.mapbox.maps.plugin.locationcomponent.LocationComponentUtils.getLocationComponent;
 import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
-import android.location.Location;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.EditorInfo;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.Priority;
-import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.android.gestures.MoveGestureDetector;
+import com.mapbox.geojson.Point;
+import com.mapbox.maps.CameraOptions;
+import com.mapbox.maps.MapView;
+import com.mapbox.maps.Style;
+import com.mapbox.maps.plugin.LocationPuck2D;
+import com.mapbox.maps.plugin.gestures.OnMoveListener;
+import com.mapbox.maps.plugin.locationcomponent.LocationComponentPlugin;
+import com.mapbox.maps.plugin.locationcomponent.OnIndicatorPositionChangedListener;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
-public class FragmentMap extends Fragment implements OnMapReadyCallback {
+public class FragmentMap extends Fragment
+        implements PermissionsListener {
+    private MapView mapView;
+    private PermissionsManager permissionsManager;
+    FloatingActionButton mylocationButton;
 
-    private static final String TAG = "FragmentMap";
-    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
-    private static final String MAPFRAGMENT_BUNDLE_KEY = "Map Fragment bundle key";
-    private ImageView btn_play;
-    private ImageButton btn_current;
-    private EditText et_search;
-    private Location currentLocation;
-    private FusedLocationProviderClient fusedClient;
-    private boolean permissionDenied = false;
-    private SupportMapFragment mapFragment;
-    private GoogleMap googleMap;
-    private LocationRequest locationRequest;
-    private LocationCallback locationCallback;
-
-    public FragmentMap() {
-        // Required empty public constructor
+    public FragmentMap (){
+        super(R.layout.fragment_map);
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        fusedClient = LocationServices.getFusedLocationProviderClient(requireActivity());
-        setupLocationUpdates();
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_map, container, false);
-        btn_current = view.findViewById(R.id.btn_current);
-        btn_play = view.findViewById(R.id.play);
-        btn_play.setOnClickListener(view1 -> {
-            Intent intent = new Intent(FragmentMap.this.getActivity(), Detect.class);
-            startActivity(intent);
-        });
-        et_search = view.findViewById(R.id.input_search);
-        mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        if (mapFragment != null) {
-            mapFragment.getMapAsync(this);
+    private final OnIndicatorPositionChangedListener onIndicatorPositionChangedListener = new OnIndicatorPositionChangedListener() {
+        @Override
+        public void onIndicatorPositionChanged(@NonNull Point point) {
+            mapView.getMapboxMap().setCamera(new CameraOptions.Builder().center(point).zoom(15.0).build());
+            getGestures(mapView).setFocalPoint(mapView.getMapboxMap().pixelForCoordinate(point));
         }
-        init();
+    };
+    private final OnMoveListener onMoveListener = new OnMoveListener() {
+        @Override
+        public void onMoveBegin(@NonNull MoveGestureDetector moveGestureDetector) {
+            getLocationComponent(mapView).removeOnIndicatorPositionChangedListener(onIndicatorPositionChangedListener);
+            getGestures(mapView).removeOnMoveListener(onMoveListener);
+            mylocationButton.show();
+        }
+
+        @Override
+        public boolean onMove(@NonNull MoveGestureDetector moveGestureDetector) {
+            return false;
+        }
+
+        @Override
+        public void onMoveEnd(@NonNull MoveGestureDetector moveGestureDetector) {
+
+        }
+    };
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (PermissionsManager.areLocationPermissionsGranted(getContext())) {
+
+        }else {
+        permissionsManager = new PermissionsManager(this);
+        permissionsManager.requestLocationPermissions(getActivity());
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_map,container,false);
+        mapView = (MapView) view.findViewById(R.id.mapbox);
+        mylocationButton = (FloatingActionButton) view.findViewById(R.id.mylocation);
+        mylocationButton.hide();
         return view;
     }
 
-    private void getLocation() {
-        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
-            return;
-        }
-        fusedClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+    //để hiển thị vị trí hiện tại và các thành phần của vị trí Mapbox thông qua location component
+    //thông qua thư viện location của mapbox
+    //nhưng ở đây sử dụng thư viện LocationComponentPlugin để kích hoạt vị trí người dùng
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mapView.getMapboxMap().loadStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
             @Override
-            public void onSuccess(Location location) {
-                if (location != null) {
-                    currentLocation = location;
-                    if (googleMap != null) {
+            public void onStyleLoaded(@NonNull Style style) {
+                mapView.getMapboxMap().setCamera(new CameraOptions.Builder().zoom(10.0).build());
+                LocationComponentPlugin locationComponentPlugin = getLocationComponent(mapView);
+                locationComponentPlugin.setEnabled(true);
+                locationComponentPlugin.setPulsingEnabled(true);
+                locationComponentPlugin.addOnIndicatorPositionChangedListener(onIndicatorPositionChangedListener);
+                getGestures(mapView).addOnMoveListener(onMoveListener);
 
-                        //TODO: THEM CAI IN POTHOLES LEN MAN HINH VI TAO VUA XOA
-
+                mylocationButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        locationComponentPlugin.addOnIndicatorPositionChangedListener(onIndicatorPositionChangedListener);
+                        getGestures(mapView).addOnMoveListener(onMoveListener);
+                        mylocationButton.hide();
                     }
-                }
+                });
             }
         });
     }
 
-    private void init() {
-        Log.d(TAG, "init:initializing");
-        et_search.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
-                if (actionId == EditorInfo.IME_ACTION_SEARCH
-                        || actionId == EditorInfo.IME_ACTION_DONE
-                        || keyEvent.getAction() == KeyEvent.ACTION_DOWN
-                        || keyEvent.getAction() == KeyEvent.KEYCODE_ENTER) {
-                    geoLocate();
-                }
-                return false;
-            }
-        });
-    }
-
-    private void geoLocate() {
-        String searchString = et_search.getText().toString();
-        Geocoder geocoder = new Geocoder(requireContext());
-        List<Address> list = new ArrayList<>();
-        try {
-            list = geocoder.getFromLocationName(searchString, 1);
-        } catch (IOException e) {
-            Log.d(TAG, "fail" + e.getMessage());
-        }
-        if (!list.isEmpty()) {
-            Address address = list.get(0);
-            LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
-            MarkerOptions markerOptions = new MarkerOptions()
-                    .position(latLng)
-                    .title("Point");
-            googleMap.addMarker(markerOptions);
-            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17));
-        }
-    }
-
+    private PermissionsListener permissionsListener;
+    //Lắng nghe các quyền cần để sử dụng cho ứng dụng
     @Override
-    public void onMapReady(GoogleMap map) {
-        googleMap = map;
-        enableMyLocation();
-        btn_current.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                getLocation();
-                CurrentPlace();
+    public void onExplanationNeeded(@NonNull List<String> list) {
 
-            }
-        });
-//        if (firebase != null)
-//        {
-//            //TODO: VUA XOA CAI IN POTHOLE LEN MAN HINH, voi xac dinh lai cai server != null
-//        }
     }
 
-    private void CurrentPlace() {
-        if (currentLocation != null) {
-            LatLng coordinate = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
-            CameraUpdate yourLocation = CameraUpdateFactory.newLatLngZoom(coordinate, 16);
-            googleMap.animateCamera(yourLocation);
-        }
-    }
-
-    @SuppressLint("MissingPermission")
-    private void enableMyLocation() {
-        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
-            return;
-        }
-        googleMap.setMyLocationEnabled(true);
-        CurrentPlace();
-    }
-
-    private void setupLocationUpdates() {
-        locationRequest = new LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 5000)
-                .setMinUpdateIntervalMillis(2000)
-                .build();
-
-        locationCallback = new LocationCallback() {
-            @Override
-            public void onLocationResult(@NonNull LocationResult locationResult) {
-                if (locationResult == null) return;
-                Location location = locationResult.getLastLocation();
-                if (location != null) {
-                    updateCameraLocation(location);
-                }
-            }
-        };
-        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-        fusedClient.requestLocationUpdates(locationRequest, locationCallback, null);
-    }
-
-    private void updateCameraLocation(Location location) {
-        if (googleMap != null) {
-            LatLng newLocation = new LatLng(location.getLatitude(), location.getLongitude());
-            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(newLocation, 15));
-        }
-    }
-
+    //Kết quả cấp phép được gọi khi người dùng có cho phép hoặc từ chối cấp phép hay không, xử lí hành động sau khi ứng dụng yêu cầu cấp phép tiếp theo
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
-            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-            return;
-        }
-        if (PermissionUtils.isPermissionGranted(permissions, grantResults, Manifest.permission.ACCESS_FINE_LOCATION)
-                || PermissionUtils.isPermissionGranted(permissions, grantResults, Manifest.permission.ACCESS_COARSE_LOCATION)) {
-            enableMyLocation();
-        } else {
-            permissionDenied = true;
-        }
-    }
+    public void onPermissionResult(boolean granted) {
+        if(granted){
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (mapFragment != null) {
-            mapFragment.onResume();
-        }
-        if (permissionDenied) {
-            showMissingPermissionError();
-            permissionDenied = false;
-        }
-    }
+        }else{
 
-    private void showMissingPermissionError() {
-        PermissionUtils.PermissionDeniedDialog.newInstance(true).show(getChildFragmentManager(), "dialog");
+        }
     }
 }
--- app/src/main/java/com/example/doan/MainActivity.java
+++ app/src/main/java/com/example/doan/MainActivity.java
@@ -1,16 +1,9 @@
 package com.example.doan;
 
-import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
